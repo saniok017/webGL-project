cd4dd84823bf1b45d6f6513f202e0f9c26bd7224
saniok017 <saniok017@gmail.com>
2019-07-04 12:33:13 +0300

    feat: - drow shader

--- src/webgl-hello-world.js
+++ src/webgl-hello-world.js
@@ -2,3 +2,43 @@ const canvas = document.querySelector('canvas');
 const gl = canvas.getContext('webgl');
 
 const program = gl.createProgram();
+
+const vertexShader = gl.createShader(gl.VERTEX_SHADER);
+const fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
+
+const vShaderSource = `
+void main() {
+  gl_PointSize = 20.0;
+  gl_Position = vec4(0, 0, 0, 1);
+}
+`;
+
+const fShaderSource = `
+  void main() {
+    gl_FragColor = vec4(1, 0, 0, 1);
+  }
+`
+
+function compileShader(shader, source) {
+  gl.shaderSource(shader, source);
+  gl.compileShader(shader);
+
+  const log = gl.getShaderInfoLog(shader);
+
+  if (log) {
+    throw new Error(log);
+  }
+}
+
+
+compileShader(vertexShader, vShaderSource);
+compileShader(fragmentShader, fShaderSource);
+
+gl.attachShader(program, vertexShader);
+gl.attachShader(program, fragmentShader);
+
+gl.linkProgram(program);
+
+gl.useProgram(program);
+
+gl.drawArrays(gl.POINTS, 0, 1);
